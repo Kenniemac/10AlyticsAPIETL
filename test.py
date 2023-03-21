e327fb537e15bc3d823766373168ac4fa6994270
Kenniemac <veevhian@gmail.com>
2023-03-21 09:26:31 +0100

    added multiply function to v2

--- test.py
+++ test.py
@@ -5,5 +5,11 @@ def add_nums():
     sum_of_num = var1 + var2
     print(f'The sum of the numbers is {sum_of_num}')
 
+def  multiply_nums():
+    # multiply 2 numbers
+    var1 = 5
+    var2 = 10
+    mult_of_num = var1 * var2
+    print(f'The multiplication of the numbers is {mult_of_num}')
 
-add_nums()
+multiply_nums()
