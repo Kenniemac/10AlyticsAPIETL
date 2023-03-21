f41b238669b5f353bcf91d29ce645a24e222f172
Kenniemac <veevhian@gmail.com>
2023-03-21 09:43:03 +0100

    added the multiplication functionality

--- test.py
+++ test.py
@@ -5,11 +5,12 @@ def add_nums():
     sum_of_num = var1 + var2
     print(f'The sum of the numbers is {sum_of_num}')
 
-def  multiply_nums():
-    # multiply 2 numbers
+def mult_nums():
+    #add 2 numbers
     var1 = 5
     var2 = 10
     mult_of_num = var1 * var2
     print(f'The multiplication of the numbers is {mult_of_num}')
 
-multiply_nums()
+
+mult_nums()
